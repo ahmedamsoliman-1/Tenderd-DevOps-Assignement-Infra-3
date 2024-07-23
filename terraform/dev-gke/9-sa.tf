cfd9060fcd297ec9d4cf4c177ef43bdb76b39a34
Ahmed Soliman <ahmed.soliman@avrioc.com>
2024-07-23 12:00:15 +0400

    AC-700 Updated in 'Khalifah A City, AE' on BRANCH 'dev' BY 'ahmed.soliman' ON 'AVRAST3412' for the files:  terraform/dev-gke/9-sa.tf

--- terraform/dev-gke/9-sa.tf
+++ terraform/dev-gke/9-sa.tf
@@ -1,18 +1,18 @@
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_service_account
-resource "google_service_account" "service-a" {
-  account_id = "service-a"
+resource "google_service_account" "service-b" {
+  account_id = "service-b"
 }
 
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_project_iam
-resource "google_project_iam_member" "service-a" {
+resource "google_project_iam_member" "service-b" {
   project = var.project_id
   role    = "roles/storage.admin"
-  member  = "serviceAccount:${google_service_account.service-a.email}"
+  member  = "serviceAccount:${google_service_account.service-b.email}"
 }
 
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_service_account_iam
-resource "google_service_account_iam_member" "service-a" {
-  service_account_id = google_service_account.service-a.id
+resource "google_service_account_iam_member" "service-b" {
+  service_account_id = google_service_account.service-b.id
   role               = "roles/iam.workloadIdentityUser"
-  member             = "serviceAccount:big-star-420419.svc.id.goog[staging/service-a]"
+  member             = "serviceAccount:big-star-420419.svc.id.goog[staging/service-b]"
 }
